2caa1fe5cf91aa7522c0af930b63326011678c54
senshi <senshi@debian-server.masatatu.com>
2013-09-22 07:15:16 +0900

    okok

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp9::Application.config.secret_token = '80146efacd96d73a865ea246c8d7ba64b52eaffe3f96b04d4310fe1460b186222c9a2f0e1c759779bfedec9e25672677816ffbb6f5325230556fe1dd05aa5a62'
